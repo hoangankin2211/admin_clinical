68ff1b5e9b281b1b16889962663fe4ff9d1645b0
hoangankin2211 <73890707+hoangankin2211@users.noreply.github.com>
2022-12-22 12:02:20 +0700

    deploy server to railway

--- lib/constants/api_link.dart
+++ lib/constants/api_link.dart
@@ -1,5 +1,6 @@
 class ApiLink {
   // static const String uri = "https://localhost:5000";
-  static const String uri = "http://localhost:5000";
+  static const String uri =
+      "https://clinical-manage-server-production.up.railway.app";
   // static const String
 }
